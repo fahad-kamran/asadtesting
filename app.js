c717e1955474f17a571bf2ad11d05a4ef9dee82f
fahad-kamran <fahadcs512@gmail.com>
2023-08-15 20:37:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+// let num1 = 3;
+// let num2 = 5;
+
+// let total = num1 + num2;
+
+// document.write('sum of ' + num1 + ' and ' + num2 + ' is ' + total);
+// document.write(`sum of ${num1} and ${num2} is ${total}`);
+
+// let num1 = 12;
+// let num2 = 3;
+// let result = num1%num2;
+// document.write(result) 
+
+// let tableNumber = 4;
+// for(let a=1; a<=10; a++){
+//     document.write(`${tableNumber} X ${a} = ${tableNumber * a}`);
+//     document.write('<br>')
+// }
+
+// let temperatureInDegree = 98;
+// // F= (C * 9/5) + 32
+
+// let temperatureInFerhenheight = (temperatureInDegree * 9/5) + 32;
+// document.write(temperatureInFerhenheight)
+
+// let dollar = 10; // per dolar 104.80
+// let riyal = 25; // per riyal 28
+
+// let pkr = dollar * 104.80 + riyal * 28;
+// document.write(pkr);
+
+
+// let tableNumber = prompt('Enter a table Number');
+// if (tableNumber === '') {
+//     for (let a = 1; a <= 10; a++) {
+//         document.write(`${5} X ${a} = ${5 * a}`);
+//         document.write('<br>')
+//     }
+// }
+// else {
+//     for (let a = 1; a <= 10; a++) {
+//         document.write(`${tableNumber} X ${a} = ${tableNumber * a}`);
+//         document.write('<br>')
+//     }
+// }
+
+
+document.write(`
+<table>
+        <tr>
+            <td>english</td>
+            <td>urdu</td>
+            <td>math</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+    </table>
+`)
